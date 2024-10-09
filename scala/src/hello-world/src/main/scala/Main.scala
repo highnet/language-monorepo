d0e22e7494d3a755b25eab919bc8b5932ba48145
Joaquin Telleria <joaquintelleria@gmail.com>
2024-10-09 18:16:40 +0200

    update handlecostumer

--- scala/src/hello-world/src/main/scala/Main.scala
+++ scala/src/hello-world/src/main/scala/Main.scala
@@ -7,12 +7,8 @@ case class Customer(id: Int, firstName: String)
 val customers =
   List(Customer(1, "John"), Customer(2, "Jane"), Customer(3, "Doe"))
 
-def getCustomer(uid: Int): Option[Customer] = {
-  customers.find(_.id == uid)
-}
-
 def handleCustomer(uid: Int): Unit = {
-  getCustomer(uid) match {
+  customers.find(_.id == uid) match {
     case Some(customer) => println(s"Customer found: ${customer.firstName}")
     case None           => println("Customer not found")
   }
